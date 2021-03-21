5ed639693511dc968f2e00a41ed76e6371b2d55b
jbaxx <jbaxx@users.noreply.github.com>
2021-03-21 05:48:05 +0000

    Improve marshmallow treatment to player objects

--- app/apis/players.py
+++ app/apis/players.py
@@ -2,13 +2,11 @@ from flask import make_response, jsonify, abort, request
 from flask_restplus import Namespace
 from flask_restplus import Resource
 from flask_restplus import fields as rest_fields
-# from flask_marshmallow import fields, ValidationError
-from marshmallow import fields, ValidationError
+from marshmallow import fields, ValidationError, Schema
 from MySQLdb import ProgrammingError
 import MySQLdb.cursors
 
 from .. import db
-from .. import ma
 
 api = Namespace('players', description= 'The players')
 
@@ -21,174 +19,107 @@ class Players(Resource):
         """
         Lists all players
         """
-        cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
-        try:
-            cursor.execute('select * from players limit 5')
-        except ProgrammingError:
-            abort(500)
-            return
-        players = cursor.fetchall()
-
+        players = PlayerModel().get_all()
         return make_response(jsonify(players), 200)
 
-    def post(self):
-        """
-        Adds a player
-        """
-        return abort(501)
-
-    def put(self):
-        """
-        Updates a player
-        """
-        return abort(501)
-
-    def delete(self):
-        """
-        Deletes a player
-        """
-        return abort(501)
-
-model = api.model('Player', {
-    'name': rest_fields.String,
-    })
-
-player_post = api.model('PlayerPost', {
-    'first_name': rest_fields.String,
-    'last_name': rest_fields.String,
-    })
-
-def return_santeno():
-    return {'name': 'luis', 'type': 'pitcher', 'age': 27}
-
-eljugador = api.model('ElJugador', {
-    'name': rest_fields.String,
-    'type': rest_fields.String,
-    'age': rest_fields.Integer,
-    })
-
-class Jugador(rest_fields.Raw):
-    def format(self, value):
-        return {'name': value.name, 'age': value.age}
-
-los_jugadores = []
 
 class PlayerModel:
     def __init__(self, **kwargs):
-                 # playerID,
-                 # birthYear,
-                 # birthMonth,
-                 # birthDay,
-                 # birthCountry,
-                 # birthState,
-                 # birthCity):
-        self.playerID = kwargs['playerID']
-        self.birthYear = kwargs['birthYear']
-        self.birthMonth = kwargs['birthMonth']
-        self.birthDay = kwargs['birthDay']
-        self.birthCountry = kwargs['birthCountry']
-        self.birthState = kwargs['birthState']
-        self.birthCity = kwargs['birthCity']
+        self.playerID = kwargs.get('playerID', '')
+        self.birthYear = kwargs.get('birthYear', '')
+        self.birthMonth = kwargs.get('birthMonth', '')
+        self.birthDay = kwargs.get('birthDay', '')
+        self.birthCountry = kwargs.get('birthCountry', '')
+        self.birthState = kwargs.get('birthState', '')
+        self.birthCity = kwargs.get('birthCity', '')
+        self.query = ''
 
-class PlayerModelSchema(ma.Schema):
-    playerID = fields.Str()
-    birthYear = fields.Str()
-    birthMonth = fields.Str()
-    birthDay = fields.Str()
-    birthCountry = fields.Str()
-    birthState = fields.Str()
-    birthCity = fields.Str()
+    def get_all(self):
+        query = """
+            SELECT
+                *
+            FROM
+                players
+            """
+        cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+        try:
+            cursor.execute(query)
+        except ProgrammingError as err:
+            raise err
+        players = cursor.fetchall()
+        return players
 
-player_model_schema = PlayerModelSchema()
+    def get_by_id(self, player_id):
+        query = """
+            SELECT
+              playerID,
+              birthYear,
+              birthMonth,
+              birthDay,
+              birthCountry,
+              birthState,
+              birthCity,
+              debut
+            FROM
+                players
+            WHERE
+                playerID = %(player_id)s
+            """
+        cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+        try:
+            cursor.execute(query, {'player_id': player_id})
+        except ProgrammingError as err:
+            raise err
+        player = cursor.fetchall()
+        return player
 
-# playerID 
-# birthYear
-# birthMonth
-# birthDay
-# birthCountry
-# birthState
-# birthCity
-# deathYear
-# deathMonth
-# deathDay
-# deathCountry
-# deathState
-# deathCity
-# nameFirst
-# nameLast
-# nameGiven
-# weight
-# height
-# bats
-# throws
-# debut
-# finalGame
-# retroID
-# bbrefID
 
-class PlayerSchema(ma.Schema):
-    num = fields.Int()
-    nombre = fields.Str()
-    apellido = fields.Str()
+class PlayerSchema(Schema):
+    playerID = fields.String()
+    birthYear = fields.String(required=True)
+    birthMonth = fields.String(required=True)
+    birthDay = fields.String(required=True)
+    birthCountry = fields.String()
+    birthState = fields.String()
+    birthCity = fields.String()
 
-player_schema = PlayerSchema()
+player_model = api.model('Player', {
+    'playerID': rest_fields.String,
+    'birthYear': rest_fields.String,
+    'birthMonth': rest_fields.String,
+    'birthDay': rest_fields.String,
+    'birthCountry': rest_fields.String,
+    'birthState': rest_fields.String,
+    'birthCity': rest_fields.String,
+    })
+
 
 @api.route('/<player_id>')
-@api.doc(params={'player_id': 'An ID'})
+@api.doc(params={'player_id': 'A player ID'})
 class Player(Resource):
     """
     Player based methods
     """
-    @api.response(200, 'Success', model)  # Api documentation
+    @api.response(200, 'Success', player_model)  # Api documentation
     @api.response(400, 'Player not found')  # Api documentation
     @api.response(500, 'Query error')  # Api documentation
-    # @api.marshal_with(eljugador)
     def get(self, player_id):
         """
         Get a player by id
         """
-        cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
         try:
-            cursor.execute("""
-                           SELECT
-                             playerID,
-                             birthYear,
-                             birthMonth,
-                             birthDay,
-                             birthCountry,
-                             birthState,
-                             birthCity,
-                             debut
-                           FROM
-                               players
-                           WHERE
-                               playerID = %(player_id)s
-                           """, {
-                               'player_id': player_id
-                               })
+            player = PlayerModel().get_by_id(player_id)
         except ProgrammingError:
             abort(500)
-            return
-        player = cursor.fetchall()
         if not player:
             return {'message': 'Player could not be found'}, 400
-        print(player[0])
-        p = PlayerModel(**player[0])
-        player_result = player_model_schema.dump(p)
 
-        # return player
-        return player_result
+        return make_response(jsonify(player), 200)
 
-        # return make_response(jsonify(player), 200)
 
-        # return make_response(jsonify(return_santeno()), 200)
-        # return return_santeno()
-
-    # weberry01
-    # @api.doc(model='ElJugador', body=Jugador)
-    # @api.expect(player_post, validate=True)
-    @api.expect(player_schema, validate=True)
-    # @api.marshal_with(eljugador)
+    @api.response(201, 'Player created')  # Api documentation
+    @api.response(422, 'Wrong body schema')  # Api documentation
+    @api.expect(player_model, validate=True)
     def post(self, player_id):
         """
         Adds a player
@@ -197,15 +128,8 @@ class Player(Resource):
         if not json_data:
             return {'message': 'No input data provided'}, 400
         try:
-            data = player_schema.load(json_data)
+            data = PlayerSchema().load(json_data)
         except ValidationError as err:
             return err.messages, 422
 
-        print(data)
-
-        return {'message': 'a player created'}
-
-        # print(api.payload)
-        # return request.form
-        # return api.payload
-        # return request.get_json(force=True)
+        return {'message': 'a player created'}, 201
